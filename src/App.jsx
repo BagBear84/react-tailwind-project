978d9cf9f827eb3a45814b518e4d040dad2f62ff
Bagbear <mm.g.a.m.e.r.1384@gmail.com>
2024-08-05 21:35:22 +0330

    working on font family and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import hamburger from './images/Buttons Group.svg'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App w-full">
+      <div className=' font-roboto'>
+        <header className=' px-4 flex items-center border-b-2 border-gray-200'>
+          <p className='py-4 font-bold text-[32px] mr-auto'>magnifico</p>
+          <button><img className=' h-fit fill-gray-900' src={hamburger} alt="" /></button>
+        </header>
+      </div>
     </div>
   );
 }
