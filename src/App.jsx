31ec73ee516152edda460fbf23185fd7eb15fedf
Bagbear <mm.g.a.m.e.r.1384@gmail.com>
2024-08-06 16:11:02 +0330

    second section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css';
 import {ReactComponent as Hamburger} from './images/Buttons Group.svg'
-import vector from './images/Vector.svg'
+import {ReactComponent as Vector} from './images/Vector.svg'
 import smartfinder from './images/1.svg'
 import shells from './images/2.svg'
 import waves from './images/3.svg'
 import artvenue from './images/4.svg'
 import video from './images/Video.png'
+import person1 from './images/Picture.png'
+import person2 from './images/Picture1.png'
+import person3 from './images/Picture2.png'
 import {ReactComponent as PlayerIcon} from './images/player-icon.svg'
 import {ReactComponent as Dote } from './images/circle-f.svg'
 import {ReactComponent as VectorWindow } from './images/Vector-window.svg'
-import { useRef } from 'react';
+import  icons  from './images/Icons.png'
+// import { useRef } from 'react';
 // import { useState } from 'react';
 
 function App() {
@@ -31,7 +35,7 @@ function App() {
             <p className='text-base font'>Amet nunc diam orci duis ut sit diam arcu, nec. Eleifend proin massa tincidunt viverra lectus pulvinar. Nunc ipsum est pellentesque turpis ultricies.</p>
             <div className='grid grid-cols-2 gap-2 justify-center text-nowrap'>
               <input className='rounded-lg border px-4 text-[16px]' type="text" placeholder='Enter work email' />
-              <button className='bg-blue-600 text-white flex items-center p-3 rounded-lg'><p className='px-4'>Sing Up Free </p><img src={vector} alt="" /></button>
+              <button className='bg-blue-600 text-white flex items-center p-3 rounded-lg'><p className='px-4'>Sing Up Free </p><Vector className='fill-white'/></button>
             </div>
             <div className='grid grid-cols-2 gap-y-4 text-gray-400 bg-gray-100 p-2 rounded-lg'>
               <p className='text-lg font-bold flex mx-auto'><img src={shells} className=' mr-1.5' alt="" />SHELLS</p>
@@ -64,6 +68,22 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+        </section>
+        <section className='px-4 my-12'>
+          <div>
+            <p className='font-bold text-[40px]'>Enjoy your time working</p>
+            <p className='my-6 text-base'>Etiam condimentum duis molestie malesuada volutpat pellentesque sed. Ornare suspendisse ut ac neque lobortis sed tincidunt. Mi tempus quis massa tellus imperdiet aenean nulla id.</p>
+            <div className='flex items-center gap-4 my-3 text-blue-600'><a href="#" className='font-medium text-base'>See how it helped others</a><Vector className='fill-blue-600'/></div>
+          </div>
+          <div className='mt-8'>
+            <div className='flex justify-between gap-2 h-64 relative'>
+              <img src={person1} className='mt-auto' alt="" />
+              <img src={person2} className='mb-auto' alt="" />
+              <img src={person3} className='my-auto' alt="" />
+              <img src={icons} className='w-44 h-44 absolute -z-10 -translate-y-1/2 -left-4 top-1/2' alt="" />
+            </div>
+            
           </div>
         </section>
       </div>
